1e54e0dfacc4f485d51ee5635e64e5c41ef21a6c
Sven van holten <sven@vanholten.com>
2022-03-28 16:02:53 +0200

    sven 28/3/22

--- sven_test.R
+++ sven_test.R
@@ -1,1 +1,27 @@
+#Libraries
+library(lmtest)
+library(glmnet)
+
+#Data loading
 data <- read_excel("datasetQ02.xlsx")
+attach(data)
+
+
+set.seed(1)
+train <- 1:78
+test <- 79:104
+y <- data$sales_1
+x <- as.matrix(data[,-1])
+
+cv.out <- cv.glmnet(x[train, ], y[train], alpha = 0, nfolds=6)
+plot(cv.out)
+bestlam <- cv.out$lambda.min
+bestlam
+
+grid <- 10^seq(10, -2, length = 100)
+ridge.mod <- glmnet(x[train, ], y[train], alpha = 1, lambda = grid, thresh = 1e-12)
+ridge.pred <- predict(ridge.mod, s = bestlam, newx = x[test, ])
+mean((ridge.pred - y[test])^2)
+
+out <- glmnet(x, y, alpha = 0)
+predict(out, type = "coefficients", s = bestlam)
