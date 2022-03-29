b21407415bcb95d941a0672d32cd63ae4985bef6
Pieter Pel <pelpieter@gmail.com>
2022-03-29 15:04:58 +0200

    asdf

--- sven_test.R
+++ sven_test.R
@@ -16,11 +16,15 @@ colnames(x.1) <- paste0(colnames(x.1),'_lag1')
 
 #Combine x and x_lag
 x_total <- cbind(x, x.1)
+x_train <- x_total[1:77]
+x_test <- x_total[78:103]
+y_train <- y[1:77]
+y_test <- y[78:103]
 
 #CV
-cv.out <- cv.glmnet(x_total[1:75], log(y), alpha = 0, nfolds=6)
+cv.out <- cv.glmnet(x_train, log(y_train), alpha = 0, nfolds=6)
 plot(cv.out)
 bestlam <- cv.out$lambda.min
 
-ridge.reg <- glmnet(x_total, log(y), alpha = 0, lambda=bestlam)
-ridge.pred <- predict(ridge.reg, s = bestlam, newx = x_total[])
+ridge.reg <- glmnet(x_train, log(y_train), alpha = 0, lambda=bestlam)
+ridge.pred <- predict(ridge.reg, s = bestlam, newx = x_test)
