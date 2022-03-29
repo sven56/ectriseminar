1f30ff953de91f986a865090d89521ad0484f7bf
Pieter Pel <pelpieter@gmail.com>
2022-03-29 16:46:21 +0200

    yo

--- sven_test.R
+++ sven_test.R
@@ -19,24 +19,28 @@ data[,2:17] <- log(data[,2:17])
 
 ###Data selection
 #Full data range
-y <- data$sales_1[2:104]
+y <- data$sales_1[3:104]
 
-colselect <- c("sales_1","sales_2","sales_3","sales_4","sales_5","sales_6","sales_7","sales_8","price_1","price_2","price_3","price_4","price_5","price_6","price_7","price_8","promo_1","promo_2","promo_3","promo_4","promo_5","promo_6","promo_7","promo_8","sales_f","promo_f","sales_s","promo_s","season")
-x <- as.matrix(data[2:104,colselect])
+colselect0 <-c("price_1","price_2","price_3","price_4","price_5","price_6","price_7","price_8","promo_1","promo_2","promo_3","promo_4","promo_5","promo_6","promo_7","promo_8","promo_f","promo_s","season") 
+  c("sales_1","sales_2","sales_3","sales_4","sales_5","sales_6","sales_7","sales_8","price_1","price_2","price_3","price_4","price_5","price_6","price_7","price_8","promo_1","promo_2","promo_3","promo_4","promo_5","promo_6","promo_7","promo_8","sales_f","promo_f","sales_s","promo_s","season")
+x.0 <- as.matrix(data[3:104],colselect0)
 
 #Lagged data range
-x.1 <- as.matrix(data[1:103,c(-1,-32)])
+colselect1 <- c("sales_1","sales_2","sales_3","sales_4","sales_5","sales_6","sales_7","sales_8","price_1","price_2","price_3","price_4","price_5","price_6","price_7","price_8","promo_1","promo_2","promo_3","promo_4","promo_5","promo_6","promo_7","promo_8","sales_f","promo_f","sales_s","promo_s")
+x.1 <- as.matrix(data[2:103],colselect1)
 colnames(x.1) <- paste0(colnames(x.1),'_lag1')
 
+colselect2 <- c("sales_1","sales_2","sales_3","sales_4","sales_5","sales_6","sales_7","sales_8","sales_f","sales_s")
+x.2 <- as.matrix(data[data[1:102],colselct2])
+
 #Combining original and lagged data
-x_total <- cbind(x, x.1)
+x_total <- cbind(x, x.1, x.2)
 
 #Combine x and x_lag
-x_total <- cbind(x, x.1)
-x_train <- x_total[1:77,]
-x_test <- x_total[78:103,]
-y_train <- y[1:77]
-y_test <- y[78:103]
+x_train <- x_total[1:76,]
+x_test <- x_total[77:102,]
+y_train <- y[1:76,]
+y_test <- y[77:102,]
 
 #CV
 cv.out <- cv.glmnet(x_train, y_train, alpha = 0, nfolds=6)
@@ -51,8 +55,8 @@ sqrt(mean((ridge.pred - y_test)^2))
 
 
       
-0_lag <- price, promo, seasonality: [3:104]
-1_lag <- alles behalve seasonality: [2:103]
-2_lag <- alles behalve seasonality, price, promo: [1:102]
+#0_lag <- price, promo, seasonality: [3:104]
+#1_lag <- alles behalve seasonality: [2:103]
+#2_lag <- alles behalve seasonality, price, promo: [1:102]
 
 
