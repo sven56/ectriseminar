796ea4f7efe13660231140db41046beb86a7020b
Sven van holten <sven@vanholten.com>
2022-03-29 15:00:07 +0200

    yo

--- sven_test.R
+++ sven_test.R
@@ -9,17 +9,10 @@ attach(data)
 data[,2:17] <- log(data[,2:17])
 
 set.seed(1)
-<<<<<<< HEAD
 y <- data$sales_1[2:104]
 x <- as.matrix(data[2:104,10:25])
 x.1 <- as.matrix(data[1:103,-1])
 colnames(x.1) <- paste0(colnames(x.1),'_lag1')
-=======
-train <- 1:78
-test <- 79:104
-y <- log(data$sales_1) 
-x <- as.matrix(data[,-1]) #Moeten nog log van de prijzen doen
->>>>>>> 7155abf96461046c6e6207e6443205a32f0cba52
 
 #Combine x and x_lag
 x_total <- cbind(x, x.1)
