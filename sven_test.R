b00f30c3b8fbfc8a6f07662f8e8b750465a0d7f6
Sven van holten <sven@vanholten.com>
2022-03-29 14:56:53 +0200

    hier pieter

--- sven_test.R
+++ sven_test.R
@@ -1,27 +1,26 @@
 #Libraries
-library(lmtest)
 library(glmnet)
 
 #Data loading
 data <- read_excel("datasetQ02.xlsx")
 attach(data)
 
+#log all of the prices
+data[,2:17] <- log(data[,2:17])
 
 set.seed(1)
-train <- 1:78
-test <- 79:104
-y <- data$sales_1
-x <- as.matrix(data[,-1])
+y <- data$sales_1[2:104]
+x <- as.matrix(data[2:104,10:25])
+x.1 <- as.matrix(data[1:103,-1])
+colnames(x.1) <- paste0(colnames(x.1),'_lag1')
 
-cv.out <- cv.glmnet(x[train, ], y[train], alpha = 0, nfolds=6)
+#Combine x and x_lag
+x_total <- cbind(x, x.1)
+
+#CV
+cv.out <- cv.glmnet(x_total[1:75], log(y), alpha = 0, nfolds=6)
 plot(cv.out)
 bestlam <- cv.out$lambda.min
-bestlam
 
-grid <- 10^seq(10, -2, length = 100)
-ridge.mod <- glmnet(x[train, ], y[train], alpha = 1, lambda = grid, thresh = 1e-12)
-ridge.pred <- predict(ridge.mod, s = bestlam, newx = x[test, ])
-mean((ridge.pred - y[test])^2)
-
-out <- glmnet(x, y, alpha = 0)
-predict(out, type = "coefficients", s = bestlam)
+ridge.reg <- glmnet(x_total, log(y), alpha = 0, lambda=bestlam)
+ridge.pred <- predict(ridge.reg, s = bestlam, newx = x_total[])
